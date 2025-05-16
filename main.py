4435f7267f57b8cdf5f40a5a38c7ea3be61433ad
DimaKovag <dimakovgan911@gmail.com>
2025-05-17 01:27:49 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def calculate_average(numbers):
+    sum = 0
+    for i in range(len(numbers)):
+        sum += numbers[i]
+    average = sum / len(numbers)
+    return average
+
+def unsafe_division(a, b):
+    return a / b
+
+def unused_function():
+    pass
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 5]
+    print("Average:", calculate_average(nums))
+    print("Division:", unsafe_division(10, 0))
